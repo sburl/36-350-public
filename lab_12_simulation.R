2ae5e29fd6f0392e806c7bd266eb8a6007ac53c9
Spencer Burleigh <spence.burleigh@gmail.com>
2018-04-17 20:07:34 -0400

    model_select function

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -11,3 +11,25 @@ generate_data = function(n, p) {
   return (results)
   
 }
+
+# **2b** Write a function `model_select(covariates, responses, cutoff)` which fits the linear regression 
+# `responses ~ covariates` and retains only those covariates whose coefficient p-values are less 
+# than or equal to `cutoff`. Then fit another regression using only the retained covariates and 
+# return the p-values from this reduced model. If there are no retained covariates return an empty
+# vector. *HINT*: You can use indexing inside of formulas: `lm(responses ~ covariates[, c(1, 2)])` 
+# will fit a regression with only the first two covariates.
+
+model_select = function(covariates, responses, cutoff) {
+  
+  coefs = summary(lm(responses ~ covariates))["coefficients"]
+  pvals = coefs[,4]
+  keep = which(pvals <= cutoff)
+  
+  if (length(keep) == 0) { return (keep) }
+  
+  keep.coefs = summary(lm(responses ~ covariates[, keep]))["coefficients"]
+  keep.vals = keep.coefs[,4]
+  
+  return (keep.vals)
+  
+}
