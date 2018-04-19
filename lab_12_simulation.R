6f9d9bed4c1a5f602d6fd3ca823c10fafefffa3b
Spencer Burleigh <spence.burleigh@gmail.com>
2018-04-18 23:49:38 -0400

    edit run_simulation and add make_plot

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -34,6 +34,39 @@ model_select = function(covariates, responses, cutoff) {
   
 }
 
+# **2c.** Write a function `run_simulation(n_trials, n, p, cutoff)` which uses the previous two functions to 
+# run `n_trials` simulations which uses data from `generate_data` in `model_select`, collects the returned 
+# p-values and displays a histogram of the p-values. Under the null hypothesis (that the regression coefficients 
+# are zero) these p-values should be uniformly distributed between 0 and 1; does this seem to be the case? 
+# Display figures for all combinations of `n = c(100, 1000, 10000)`, `p = 10, 20, 50` and set `cutoff = 0.05`.
+
+run_simulation = function(n_trials, n, p, cutoff) {
+  
+  pvals = vector(mode = "list", length = n_trials)
+  
+  # run trials 
+  for (i in 1:n_trials) {
+    
+    run = generate_data(n, p)
+    pvals[i] = model_select(run$covariates, run$responses, run$cutoff)
+    
+  }
+  
+  save(pvals, file = "pvalues.rdata")
+  
+}
+
+# **2d.** One problem with simulations is that they can take a long time to run. This can be a problem if you 
+# merely want to change the resulting figure (maybe there was a typo). Thus a better strategy is to separate 
+# the simulation and plotting functions. Edit your previous code to save the p-values to a file and add a 
+# function `make_plot(datapath)` which reads the data from `datapath` and makes the plot.
+
+make_plot = function (datapath) {
+  
+  load(datapath)
+  hist(x = pvals, main = "P-Value Distribution", xlab = "P-Values", ylab = "Frequency")
+  
+}
 
 
 
